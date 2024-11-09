4e0275396c0a1b0df7781468b1569bb88f9a73f1
Sandesh Shinde <sandeshshinde7047@gmail.com>
2024-11-09 11:47:50 +0530

    Page 1 mobile view changed

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -46,27 +46,28 @@ function Page1() {
 
   return (
     <>
-      <div style={gradientStyle} className="relative z-0 overflow-hidden h-screen md:h-[150vh]">
-        
-        {/* Link and Logo */}
-        <a href="https://www.wcewlug.org/" className="absolute right-5 top-3 z-[600]">
-          <img className="w-[10vh] md:w-[18vh]" src="wlug.png" alt="WLUG Logo" />
-        </a>
+      <div style={gradientStyle} className="relative z-0 overflow-hidden h-screen">
+  
+      {/* Link and Logo */}
+      <a href="https://www.wcewlug.org/" className="absolute right-5 top-3 z-[600]">
+        <img className="w-[10vh] md:w-[18vh]" src="wlug.png" alt="WLUG Logo" />
+      </a>
 
-        {/* Background Images */}
-        <img className="stars absolute top-0 left-0 w-full z-10" src="stars.png" alt="Stars" />
-        <img className="moon absolute top-0 right-0 w-full z-20 mix-blend-screen" src="moon.png" alt="Moon" />
-        <img className="mountains absolute -top-0 left-0 w-full md:left-48 z-35" src="mountains.svg" alt="Mountains" />
-        
-        {/* Heading Text */}
-        <div className="text text-white font-bold text-[2vh] md:text-[10vh] text-center z-30 pt-32 md:pt-48 whitespace-nowrap">
-          Walchand Linux Users' Group
-        </div>
-
-        {/* Front Trees */}
-        <img className="trees absolute top-0 left-0 w-full z-40" src="front_trees.svg" alt="Front Trees" />
-        
+      {/* Background Images Container */}
+      <div className="absolute top-0 left-0 w-full h-full">
+        <img className="stars absolute top-0 left-0 w-full h-full object-cover z-10" src="stars.png" alt="Stars" />
+        <img className="moon absolute top-0 right-0 w-full h-full object-cover z-20 mix-blend-screen" src="moon.png" alt="Moon" />
+        <img className="mountains absolute top-0 left-0 w-full h-full object-cover md:left-48 z-35" src="mountains.svg" alt="Mountains" />
+        <img className="trees absolute top-0 left-0 w-full h-full object-cover z-40" src="front_trees.svg" alt="Front Trees" />
       </div>
+
+      {/* Heading Text */}
+      <div className="text text-white font-bold text-[3vh] md:text-[10vh] text-center z-30 pt-72 md:pt-48 whitespace-nowrap">
+        Walchand Linux Users' Group
+      </div>
+
+    </div>
+
     </>
   );
 }
