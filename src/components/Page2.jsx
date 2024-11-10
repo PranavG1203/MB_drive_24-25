2b0103ed389851f00f7187a2bd151d8cdde631c9
PranavG1203 <pranavg2016@gmail.com>
2024-11-10 18:20:02 +0530

    tux

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -3,6 +3,7 @@ import RegisterPage from "./RegisterPage";
 import ModelTux from "./ModelTux";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+// import "./Page2.css";s
 
 function Page2() {
   return (
@@ -14,16 +15,17 @@ function Page2() {
         {/* <div className="heading text-white font-bold text-xl md:text-[4vh]"></div> */}
         <div className="heading text-white text-[1.2vh] text-xl md:text-[2.5vh] text-center">
           <p>
-          Prepare to join a vibrant community of Linux enthusiasts and immerse yourself in the dynamic world of Open Source.
+            Prepare to join a vibrant community of Linux enthusiasts and immerse
+            yourself in the dynamic world of Open Source.
           </p>
         </div>
       </div>
 
-      <div className="box flex flex-col md:flex-row gap-4 w-full md:w-[80vw] rounded-xl">
+      <div className="box flex flex-col md:flex-row gap-4 w-full md:w-[80vw] h-[120vh] rounded-xl ">
         {/* Left Box with Canvas and Model */}
-        <div className="tux flex-1 flex justify-center items-center w-full md:w-[50%] bg-opacity-[20%] bg-zinc-400 rounded-xl">
-          <Canvas className="w-[90vw] h-[120vh] md:w-full md:h-full">
-            <ambientLight  />
+        <div className="tux flex-1 flex justify-center items-center w-full md:w-[50%] bg-opacity-[20%] bg-zinc-400 rounded-xl ">
+          <Canvas className="w-[70vw] h-[250vh] md:w-full md:h-full">
+            <ambientLight />
             <ModelTux />
             <OrbitControls enableZoom={false} enablePan={true} />
           </Canvas>
