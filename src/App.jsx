83bf1f00c472945a9ae20c4ceaa50425b3aba2a8
PranavG1203 <pranavg2016@gmail.com>
2024-11-10 22:49:33 +0530

    redirection

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import React from 'react';
-import RegisterPage from './components/RegisterPage';
-import Page1 from './components/Page1';
-import Page2 from './components/Page2';
-import Footer from './components/Footer';
-import { Text } from '@react-three/drei';
-import './App.css'
+import React from "react";
+import RegisterPage from "./components/RegisterPage";
+import Page1 from "./components/Page1";
+import Page2 from "./components/Page2";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   const gradientStyle = {
-    minHeight: '100vh',
-    background: 'linear-gradient(#2b1055, #7597de)',
-    backgroundAttachment: 'fixed',
-};
-
+    minHeight: "100vh",
+    background: "linear-gradient(#2b1055, #7597de)",
+    backgroundAttachment: "fixed",
+  };
 
   return (
     <>
-    <div style={gradientStyle} className='h-full '>
-      <Page1 />
-      <Page2/>
-      
-      <Footer/>
-    </div>
+      <div style={gradientStyle} className="h-full">
+        <Page1 />
+
+        {/* Assign an id to Page2 */}
+        <div id="Page2">
+          <Page2 />
+        </div>  
+
+        <Footer />
+      </div>
     </>
   );
 }
